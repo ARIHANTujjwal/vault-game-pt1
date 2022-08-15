a12a9e502f278cad3de3f84076d648b2a46b6ca1
ARIHANTujjwal <87779315+ARIHANTujjwal@users.noreply.github.com>
2022-08-15 10:22:31 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "12 3 4 5 6 7 8 9 10";
+const accessCode2 = "GOLD BARS";
+const accessCode3 = "ARRAY";
+
+function clues() {
+    
+    fill("black")
+    textSize(15)
+    text("10 3 6 4 1 2 5 7 9 8", 100,50)
+    fill("black")
+    text("Hint: write from least to greatest and put spaces between numbers!", 100,70)
+
+    fill("black")
+    textSize(15)
+    text("O L G D  ARBS", 700,150)
+    fill("black")
+    text("Hint: Something shiny that is inside a vault!", 700,170)
+
+    fill("black")
+    textSize(15)
+    text("R R Y A A", 100,250)
+    fill("black")
+    text("Hint: SOMTING RELATED TO CNDING THAT IS USED TO STORE VALUES!", 100,270)
+
+}
